083b460ebe17a29dd5bf22e7b037cfa0427de865
Yang <didtjdwnsv3@naver.com>
2021-04-10 13:23:53 +0900

    Setting

--- AutoTraderGUI/AutoTraderGUI/Library/SettingsInfo.cs
+++ AutoTraderGUI/AutoTraderGUI/Library/SettingsInfo.cs
@@ -9,16 +9,16 @@ namespace AutoTraderGUI.Library
     [Serializable]
     class SettingsInfo
     {
-        public string InterpreterPath;
-        public string APICollectorPath;
-        public string DartCollectorPath;
-        public int MaxRequestsCount;
-        public float RequestsInterval;
-        public string DartAPI;
-        public string DBIP;
-        public int DBPort;
-        public string DBID;
-        public string DBPW;
+        public string InterpreterPath = "D:\\Anaconda\\python.exe";
+        public string APICollectorPath = "C:\\Users\\양성준\\Source\\Repos\\GraduationProject\\APICollector\\APICollector.py";
+        public string DartCollectorPath = "C:\\Users\\양성준\\Source\\Repos\\GraduationProject\\APICollector\\DartCollector.py";
+        public int MaxRequestsCount = 1000;
+        public float RequestsInterval = 0.4f;
+        public string DartAPI = "085990c5ee309d660c0e658abaeb455e7454eecb";
+        public string DBIP = "localhost";
+        public int DBPort = 3306;
+        public string DBID = "root";
+        public string DBPW = "vmfhwprxm1";
 
     }
 }
